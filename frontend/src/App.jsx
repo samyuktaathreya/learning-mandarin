a606b65b35c48e4ac20cc5492dd73f5238a180a4
Samyukta Athreya <athreya.samyukta@gmail.com>
2026-07-23 18:00:13 -0500

    mandarin voice agent is a successgit add .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DuolingoStyleQuestions from "./pages/DuolingoStylePractice"
+import MandarinVoicePractice from "./pages/MandarinVoicePractice"
 import TestPronunciation from "./pages/TestPronounciation"
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/duolingo-style-practice" element={<DuolingoStyleQuestions />} />
+        <Route path="/mandarin-voice-practice" element={<MandarinVoicePractice />} />
         <Route path="/test" element={<TestPronunciation />} />
         <Route path="*" element={<div>I am lost! Current path: {window.location.pathname}</div>} />
       </Routes>
