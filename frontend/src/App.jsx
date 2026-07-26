ceaa96937bd2c79421fbff075b94c4e83506f1e1
Samyukta Athreya <athreya.samyukta@gmail.com>
2026-07-26 16:41:22 -0500

    grammar tips added and they look pretty and cleanly formatted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DuolingoStyleQuestions from "./pages/DuolingoStylePractice"
 import MandarinVoicePractice from "./pages/MandarinVoicePractice"
 import TestPronunciation from "./pages/TestPronounciation"
+import DebugGrammarTips from "./pages/DebugGrammarTips"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/duolingo-style-practice" element={<DuolingoStyleQuestions />} />
         <Route path="/mandarin-voice-practice" element={<MandarinVoicePractice />} />
         <Route path="/test" element={<TestPronunciation />} />
+        <Route path="/debug-grammar-tips" element={<DebugGrammarTips />} />
         <Route path="*" element={<div>I am lost! Current path: {window.location.pathname}</div>} />
       </Routes>
     </BrowserRouter>
