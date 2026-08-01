e32519d436044a188b6bd26a70ca629fa4f32e58
Samyukta Athreya <athreya.samyukta@gmail.com>
2026-08-01 10:57:16 -0500

    config files updated, data pipelines next

---
+++ app/core/config/session.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+from .shared import DATA_DIR
+
+OUTPUT_DB_PATH = DATA_DIR / "mandarin_app.db"
--- app/shared/models.py
+++ app/shared/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, Float, DateTime, UniqueConstraint, String, Text
 from sqlalchemy.dialects.sqlite import TEXT
-from database import Base
+from core.database import Base
 from datetime import datetime
 
 class DictionaryEntry(Base):
