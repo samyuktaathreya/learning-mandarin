9c3ae216c0cabf6558f8144266c2dc0f29096d25
Samyukta Athreya <athreya.samyukta@gmail.com>
2026-08-29 15:57:35 -0400

    debugging frontend deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const isProd = env.VITE_ENVIRONMENT === 'PROD' || mode === 'production'
-
-  return {
-    plugins: [react()],
-    // Replace YOUR-REPO-NAME with your actual repository name:
-    base: isProd ? '/' : '/',
-    server: {
-      host: '0.0.0.0',
-      port: 5173,
-      proxy: {
-        '/api': 'http://127.0.0.1:8000',
-      },
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/api': 'http://127.0.0.1:8000',
     },
-  }
+  },
 })
